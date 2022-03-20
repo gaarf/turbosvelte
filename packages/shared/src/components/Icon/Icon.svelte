5f1d14d51569d6807ab371b9bf850bb624bd8494
Adrien <gaarf@users.noreply.github.com>
2022-03-20 16:53:16 -0700

    rebuild dep

--- packages/shared/src/components/Icon/Icon.svelte
+++ packages/shared/src/components/Icon/Icon.svelte
@@ -2,23 +2,22 @@
   @component an SVG icon
  -->
 <script lang="ts">
-	import Fa from 'svelte-fa';
 	import iconDefs, { type IconName } from './icons';
 	import type { ComponentProps } from '$utils/types';
 
-	type IconSize = ComponentProps<Fa>['size'];
+	// type IconSize = ComponentProps<Fa>['size'];
 
 	interface $$Props {
 		name: IconName;
-		size?: IconSize;
+		// size?: IconSize;
 		label?: string;
 	}
 
 	export let name: IconName;
-	export let size: IconSize = '1x';
+	// export let size: IconSize = '1x';
 	export let label = '';
 </script>
 
 <span aria-label={label || name}>
-	<Fa icon={iconDefs[name]} {size} />
+	{@html iconDefs[name]}
 </span>
