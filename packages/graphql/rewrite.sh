e27ca4dd9194c1369c3b172596186c6264ac0531
Adrien <gaarf@users.noreply.github.com>
2022-03-21 17:44:01 -0700

    waaa

--- packages/graphql/rewrite.sh
+++ packages/graphql/rewrite.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-if [ -z "$1" ]
-  then
-    >&2 echo "No argument supplied"
-    exit 1
-fi
->&2 echo in `pwd`
->&2 echo "Rewriting first line of $1 ..."
-sed -i '' -e '1s/import { TypedDocumentNode/import type { TypedDocumentNode/' $1
+sed -i '' -e '1s/import { TypedDocumentNode/import type { TypedDocumentNode/' build/index.ts
