76c91dd62f50968d6bf0f5871b1aeecafe9a2de3
Adrien <gaarf@users.noreply.github.com>
2022-03-21 17:29:05 -0700

    add debug

--- packages/graphql/rewrite.sh
+++ packages/graphql/rewrite.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+echo "Rewriting first line of $1 ..."
 sed -i '' -e '1s/import { TypedDocumentNode/import type { TypedDocumentNode/' $1
