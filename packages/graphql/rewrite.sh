e632d6675307d0d0a1dd3e504f1f2ea563974b9e
Adrien <gaarf@users.noreply.github.com>
2022-03-21 17:40:43 -0700

    wtf2

--- packages/graphql/rewrite.sh
+++ packages/graphql/rewrite.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]
   then
-    echo "No argument supplied"
+    >&2 echo "No argument supplied"
     exit 1
 fi
-echo in `pwd`
-echo "Rewriting first line of $1 ..."
+>&2 echo in `pwd`
+>&2 echo "Rewriting first line of $1 ..."
 sed -i '' -e '1s/import { TypedDocumentNode/import type { TypedDocumentNode/' $1
